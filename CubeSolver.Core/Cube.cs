f6bedc557c4beea96610a6a090b408921891cca0
Vlad Kabalov <vladskryto@gmail.com>
2019-07-28 20:11:45 +0400

    Init commit

---
+++ CubeSolver.Core/Cube.cs
@@ -0,0 +1,49 @@
+﻿namespace RubixCubes.Core
+{
+    public class Cube
+    {
+        Side Green = new Side();
+        Side Red = new Side();
+        Side White = new Side();
+        Side Blue = new Side();
+        Side Yellow = new Side();
+        Side Orange = new Side();
+
+        public Cube()
+        {
+
+        }
+
+        public void Rotate(Turn turn, int count)
+        {
+
+        }
+
+        public class Side
+        {
+            readonly Color[,] _sideMatrix = new Color[3, 3];
+
+            public Color this[int column, int row] { get => _sideMatrix[column, row]; set => _sideMatrix[column, row] = value; };
+        }
+
+        public enum Turn
+        {
+            Left,
+            Right,
+            Up,
+            Down,
+            Front,
+            Back
+        }
+
+        public enum Color
+        {
+            Red,
+            Green,
+            White,
+            Blue,
+            Yellow,
+            Orange
+        }
+    }
+}
---
+++ CubeSolver.Core/CubeGenerator.cs
@@ -0,0 +1,6 @@
+﻿namespace RubixCubes.Core
+{
+    class CubeGenerator
+    {
+    }
+}
